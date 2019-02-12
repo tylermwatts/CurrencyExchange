e41758b1f267b44c093f0e04d50b2b28312794f8
Tyler <tylermwatts@gmail.com>
2019-02-12 10:51:14 -0600

    added test project

---
+++ CurrencyExchange.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CurrencyExchange.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
