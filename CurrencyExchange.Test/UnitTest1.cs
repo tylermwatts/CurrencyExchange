486385939e54dc5280e3d4c76acce20e8a2b324d
Tyler Watts <tylermwatts@gmail.com>
2019-02-12 20:25:04 -0600

    wip unit tests

--- CurrencyExchange.Test/UnitTest1.cs
+++ CurrencyExchange.Test/UnitTest1.cs
@@ -1,4 +1,7 @@
+using CurrencyExchange.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CurrencyExchange.Test
 {
@@ -6,8 +9,23 @@ namespace CurrencyExchange.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CurrencyInfoStatic_Returns_Correct_CurrencyInfo()
         {
+            var expected = new CurrencyInfo { Name = "Euros", Symbol = "€" };
+            var result = CurrencyInfoStatic.GetCurrencyInfo(CurrencyType.EUR);
+
+            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(expected.Symbol, result.Symbol);
+        }
+        [TestMethod]
+        public async Task CurrencyExchangeService_Gets_Correct_InitialType_and_ReturnType()
+        {
+            var client = new HttpClient();
+            CurrencyExchangeService exchangeService = new CurrencyExchangeService(client);
+            var exchangeData = await exchangeService.GetExchangeDataAsync(5, CurrencyType.USD, CurrencyType.GBP);
+
+            Assert.AreEqual(exchangeData.InitialCurrency, CurrencyType.USD);
+            Assert.AreEqual(exchangeData.ReturnCurrency, CurrencyType.GBP);
         }
     }
 }
